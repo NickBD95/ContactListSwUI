5134b4bedba4cedb044e74e6c8ff53a893d5d760
Nickolai Grachev <nickbd95@gmail.com>
2023-12-27 10:13:06 +0300

    Initial Commit

---
+++ ContactListSwUI/ContactListSwUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwUIApp.swift
+//  ContactListSwUI
+//
+//  Created by Nikolai Grachev on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
